0f3795ca5d08778d4ee3649901f1724539cadf08
bigtea <109420474+philip-ttz@users.noreply.github.com>
2024-06-04 22:30:41 +0200

    function playRound

--- javascript.js
+++ javascript.js
@@ -14,5 +14,42 @@ function getHumanChoice(){
     return choice;
 }
 
+function playRound(humanChoice, computerChoice){
+    let humanChoice = humanChoice.toLowerCase();
+    if(humanChoice === "rock"){
+        if(computerChoice === "rock"){
+            console.log("Draw, you both chose rock");
+        }else if(computerChoice === "paper"){
+            console.log("Loss, paper beats rock");
+            computerChoice++;
+        }else if(computerChoice === "scissor"){
+            console.log("Win, rock beats scissor");
+            humanChoice++;
+        }
+    }else if(humanChoice === "paper"){
+        if(computerChoice === "rock"){
+            console.log("Win, paper beats rock");
+            humanChoice++;
+        }else if(computerChoice === "paper"){
+            console.log("Draw, you both chose paper");
+        }else if(computerChoice === "scissor"){
+            console.log("Loss, scissor beats paper");
+            computerChoice++;
+        }
+    }else if(humanChoice === "scissor"){
+        if(computerChoice === "rock"){
+            console.log("Loss, rock beats scissor");
+            computerChoice++;
+        }else if(computerChoice === "paper"){
+            console.log("Win, scissor beats paper");
+            humanChoice++;
+        }else if(computerChoice === "scissor"){
+            console.log("Draw, you both chose scissor");
+        }
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
+const humanSelection = getHumanChoice();
+const copmuterSelection = getComputerChoice();
