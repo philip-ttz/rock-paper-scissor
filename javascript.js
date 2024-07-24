e74f033b1b76cdc901c67f5d9e34fd57c65fb365
bigtea <109420474+philip-ttz@users.noreply.github.com>
2024-07-24 21:09:07 +0200

    fixing game and making first css

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 const rockBtn=document.querySelector('.rock');
 const paperBtn=document.querySelector('.paper');
 const scissorBtn=document.querySelector('.scissor');
+const gameModal=document.querySelector('.game-modal');
+const playAgain=document.querySelector(".play-again");
+let computerScore=0;
+let humanScore=0;
+let winningScore=5;
+
 
 rockBtn.addEventListener('click', () =>{
     playRound('rock', getComputerChoice());
@@ -11,6 +17,7 @@ paperBtn.addEventListener('click', () =>{
 scissorBtn.addEventListener('click', () =>{
     playRound('scissor', getComputerChoice());
 })
+playAgain.addEventListener('click', resetGame)
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
@@ -19,26 +26,20 @@ function getComputerChoice(){
     } else if(choice==1){
         return "paper";
     } else {
-        return "scissors";
+        return "scissor";
     }
-}
-
-function getHumanChoice(){
-    let choice = prompt("What is your choice");
-    let returnchoice = choice.toLowerCase();
-    return returnchoice;
 }
 
 function playRound(humanChoice, computerChoice){
     console.log(humanChoice,computerChoice);
     
-    /*if(humanChoice === "rock"){
+    if(humanChoice === "rock"){
         if(computerChoice === "rock"){
             console.log("Draw, you both chose rock");
         }else if(computerChoice === "paper"){
             console.log("Loss, paper beats rock");
             computerScore++;
-        }else if(computerChoice === "scissors"){
+        }else if(computerChoice === "scissor"){
             console.log("Win, rock beats scissor");
             humanScore++;
         }
@@ -48,38 +49,43 @@ function playRound(humanChoice, computerChoice){
             humanScore++;
         }else if(computerChoice === "paper"){
             console.log("Draw, you both chose paper");
-        }else if(computerChoice === "scissors"){
+        }else if(computerChoice === "scissor"){
             console.log("Loss, scissor beats paper");
             computerScore++;
         }
-    }else if(humanChoice === "scissors"){
+    }else if(humanChoice === "scissor"){
         if(computerChoice === "rock"){
             console.log("Loss, rock beats scissor");
             computerScore++;
         }else if(computerChoice === "paper"){
             console.log("Win, scissor beats paper");
             humanScore++;
-        }else if(computerChoice === "scissors"){
+        }else if(computerChoice === "scissor"){
             console.log("Draw, you both chose scissor");
         }
-    }*/
+    }
+    console.log(humanScore,computerScore);
+    if(humanScore===winningScore || computerScore===winningScore){
+        if(humanScore>computerScore){
+            gameOver(true);
+        }else{
+            gameOver(false);
+        }
+    }
 }
 
-function playGame(anz){
-    let humanScore = 0;
-    let computerScore = 0;
 
-    for(let i=0; i<anz; i++){
-        const humanSelection = getHumanChoice();
-        const copmuterSelection = getComputerChoice();      
-        playRound(humanSelection, copmuterSelection);
-    }
 
-    if(humanScore>computerScore){
-        console.log("You win");
-    }else if(humanScore<computerScore){
-        console.log("You lose");
-    }else{
-        console.log("Draw");
-    }
+function gameOver(isWon){
+    const modalText = isWon ? `You won by` : `The computer won by`;
+    gameModal.querySelector("h4").innerText=`${isWon ? 'Congratulations!' : 'Game Over!'}`;
+    gameModal.querySelector('p').innerHTML=`${modalText} <b>${humanScore} / ${computerScore}`;
+    gameModal.classList.add("show");
+}
+
+function resetGame(){
+    humanScore=0;
+    computerScore=0;
+    console.log(humanScore, computerScore);
+    gameModal.classList.remove('show');
 }
